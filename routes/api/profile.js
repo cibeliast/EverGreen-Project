39f24a347a357a85403811f413119c48ed6da755
ASUS <ASUS@LAPTOP-3HEA4LNI>
2025-06-05 20:58:30 +0700

    Add page student profile and profile form (unfixed)

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -31,4 +31,31 @@ router.get('/profile', (req, res) => {
     });
 });
 
+router.post('/profile/update', (req, res) => {
+    if(!req.session.userId) {
+        return res.status(401).json({ error: 'Unauthorized'});
+    }
+
+    const userId = req.session.userId;
+    const role = req.session.role;
+    const {name, username, email, address, phone_number} = req.body;
+
+    let idColumn;
+    if (role === 'students') {
+        idColumn = 'student_id';
+    } else if (role === 'teachers') {
+        idColumn = 'teacher_id';
+    } else {
+        return res.status(400).json({ error: 'Invalid Role'});
+    }
+
+    const query = `UPDATE ${role} SET name=?, username=?, email=?, address=?, phone number=? WHERE ${idColumn}=?`;
+    db.query(query, [name, username, email, address, phone_number, userId], (err, result) => {
+        if (err) {
+            return res.status(500).json({ error: 'Database error'});
+        }
+        res.json({success: true});
+    });
+});
+
 export default router;
