09405f727f87e0b7a42b814ecc6073a9f439499a
avaiis <154651371+avaiis@users.noreply.github.com>
2025-06-07 17:17:58 +0700

    read data from table (sorting day still failed)

--- routes/teacher/all_schedule.js
+++ routes/teacher/all_schedule.js
@@ -10,7 +10,26 @@ router.get('/all_schedule', (req, res) => {
         return res.status(403).json({ message: 'Unauthorized'});
     }
 
-    const sql = 'SELECT sc.day, sc.time, tc.name AS teacher_name, st.name AS student_name FROM schedules sc JOIN teachers tc ON sc.teacher_id = tc.id JOIN students st ON sc.student_id = st.id';
+    const sql = `
+        SELECT 
+        sc.day, sc.time, 
+        tc.name AS teacher_name, 
+        st.name AS student_name
+        FROM schedules sc
+        JOIN teachers tc ON sc.teacher_id = tc.teacher_id
+        JOIN students st ON sc.student_id = st.student_id
+        ORDER BY
+        CASE sc.day
+            WHEN 'Monday' THEN 1
+            WHEN 'Tuesday' THEN 2
+            WHEN 'Wednesday' THEN 3
+            WHEN 'Thursday' THEN 4
+            WHEN 'Friday' THEN 5
+            WHEN 'Saturday' THEN 6
+            WHEN 'Sunday' THEN 7
+            ELSE 999
+        END;
+`;
 
     db.query(sql, (err, results) => {
         if(err){
