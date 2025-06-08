1bf3c9248559aa72e9f35d431599c36d5c03a8dc
avaiis <154651371+avaiis@users.noreply.github.com>
2025-06-08 12:25:09 +0700

    balikin route student hdeh,,,,,,,

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.use('/', authRoutes);
 app.use('/', dashboard);
 app.use('/api', profileRouter);
 app.use('/api', studentSchedule);
-app.use('/api', students);
+app.use('/teacher', students);
 app.use('/', changePassword);
 app.use('/api', topicsRouter);
 app.use('/api', quizzesRouter);
@@ -63,7 +63,7 @@ app.get('/quiz', (req, res) => {
 
 // Sidebar guru ke 
 // 1. Students
-app.get('/students', (req, res) => {
+app.get('/teacher/students', (req, res) => {
   res.sendFile(path.join(__dirname, 'pages/teacher/students.html'));
 });
 
