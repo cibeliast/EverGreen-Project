af75c00d59b689c3d320210fec575b792b0ff23c
cibeliast <cindynabella29@gmail.com>
2025-07-02 10:14:11 +0700

    Ganti creat connection jd pke pool biar g ke sleep

--- db/db.js
+++ db/db.js
@@ -1,23 +1,40 @@
+// POOL CONNECTION
 import mysql from 'mysql2';
 
-const db = mysql.createConnection({
-    // host: 'localhost',
-    // user: 'root',
-    // password: '',
-    // database: 'rpl_kk'
+const db = mysql.createPool({
     host: 'maglev.proxy.rlwy.net',
     user: 'root',
     password: 'YxCIPIxdOkzlxNlyEQtJfUgiYNGcsZKt',
     database: 'railway',
-    port: '11975'
-});
-
-db.connect((err) => {
-    if (err) {
-        console.error('Failed connect to MySQL: ', err);
-        return;
-    }
-    console.log('Connected to MySQL');
+    port: 11975, 
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
 export default db;
+
+// CREATE CONNECTION
+// import mysql from 'mysql2';
+
+// const db = mysql.createConnection({
+//     // host: 'localhost',
+//     // user: 'root',
+//     // password: '',
+//     // database: 'rpl_kk'
+//     host: 'maglev.proxy.rlwy.net',
+//     user: 'root',
+//     password: 'YxCIPIxdOkzlxNlyEQtJfUgiYNGcsZKt',
+//     database: 'railway',
+//     port: '11975'
+// });
+
+// db.connect((err) => {
+//     if (err) {
+//         console.error('Failed connect to MySQL: ', err);
+//         return;
+//     }
+//     console.log('Connected to MySQL');
+// });
+
+// export default db;
